865a0b0e0d44f2761f3233680f5e21962c7a0c02
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-09-23 11:49:16 +0300

    Separate component from html

--- Web-App/src/App.jsx
+++ Web-App/src/App.jsx
@@ -1,7 +1,17 @@
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import Login from './components/login/Login';
+
 function App() {
-    return <>
-    <h1>WEB APP</h1>
-    </>;
+    return (
+        <div id="box">
+            <Header />
+            <main id="main-content">
+                <Home />
+                {/* <Login /> */}
+            </main>
+        </div>
+    );
 }
 
 export default App;
