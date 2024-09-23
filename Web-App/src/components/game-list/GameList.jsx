499a200f3c7c7ed929036263768ddac03de92bb3
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-09-23 16:05:23 +0300

    implement game details

--- Web-App/src/components/game-list/GameList.jsx
+++ Web-App/src/components/game-list/GameList.jsx
@@ -1,24 +1,28 @@
 import { useEffect, useState } from 'react';
+
 import * as gamesAPI from '../../api/games-api';
+
 import GameListItem from './game-list-item/GameListItem';
 
 export default function GameList() {
     const [games, setGames] = useState([]);
 
     useEffect(() => {
-        gamesAPI.getAll().then((result) => setGames(result));
-    }, []);
+        (async () => {
+            const result = await gamesAPI.getAll();
+            console.log(result);
 
-    // console.log(games);
+            setGames(result);
+        })();
+    }, []);
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
             {/* <!-- Display div: with information about every game (if any) --> */}
-            {games.length > 0 ? (
-                games.map((game) => <GameListItem key={game._id} {...game} />)
-            ) : (
-                <h3 className="no-articles">No articles yet</h3>
-            )}
+            {games.length > 0
+                ? games.map((game) => <GameListItem key={game._id} {...game} />)
+                : <h3 className="no-articles">No articles yet</h3>
+            }
 
             {/* <!-- Display paragraph: If there is no games  --> */}
         </section>
