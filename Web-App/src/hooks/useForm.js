2bb513e9700ad651900841814fb5232253c71a7e
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-10-07 22:46:44 +0300

    clear form after submit

--- Web-App/src/hooks/useForm.js
+++ Web-App/src/hooks/useForm.js
@@ -14,6 +14,7 @@ export function useForm(initialValues, submitCallback) {
     const submitHandler = (e) => {
         e.preventDefault();
         submitCallback(values);
+        setValues(initialValues)
     };
 
     return {
